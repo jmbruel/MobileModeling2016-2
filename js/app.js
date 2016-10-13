7b9c7f1e3c03a44e832657065a5fdde1a3e16208
Keyvan E <astaen@outlook.fr>
2016-10-13 12:16:50 +0200

    Commit

--- js/app.js
+++ js/app.js
@@ -1,23 +1,3 @@
-/*$(document).ready(function() {
-	var page = $('.entry-content').data("default");
-	loadContent(page);
-
-	$('nav a').click(function() {
-		var page = $(this).data("template");
-		loadContent(page);
-	})
+$(document).ready(function() {
+	 $(".button-collapse").sideNav();
 })
-
-function loadContent(page) {
-	console.log(page);
-	jQuery.ajax({
-	  url: 'vues/'+page,
-	  type: 'GET',
-	  success: function(data, textStatus, xhr) {
-	    $('.entry-content').html(data);
-	  },
-	  error: function(xhr, textStatus, errorThrown) {
-	    alert("Error loading template");
-	  }
-	});	
-}*/
